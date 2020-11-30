2673e72cd53174cdec2873e7d1e6443016a3e880
marsipu <dev@earthman-music.de>
2020-11-30 16:34:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Streamlit MNE-Python Demo
+#### A quick demo to see if MNE-Python is running in streamlit
---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import mne
+from os.path import join
+
+@st.cache
+def load_raw():
+    sample_data_folder = mne.datasets.sample.data_path()
+    sample_data_raw_file = join(sample_data_folder, 'MEG', 'sample',
+                                'sample_audvis_filt-0-40_raw.fif')
+    loaded_raw = mne.io.read_raw_fif(sample_data_raw_file, preload=True)
+
+    return loaded_raw
+
+
+st.title('MNE-Demo')
+
+highpass = st.sidebar.slider('Set Highpass', min_value=0, max_value=75, value=1)
+lowpass = st.sidebar.slider('Set Lowpass', min_value=0, max_value=75, value=50)
+
+raw = load_raw()
+
+raw_figure = raw.plot()
+st.write(raw_figure)
+
+filtering_state = st.text('Filtering data...')
+with st.spinner(text='Filtering'):
+    raw_filtered = raw.copy().filter(highpass, lowpass)
+filtering_state.text('Filtering done!')
+
+figure = raw_filtered.plot_psd(show=False)
+st.write(figure)
+
+st.balloons()
