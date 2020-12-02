80ed76894106b32494754298d2f19c6e0d80d53b
marsipu <dev@earthman-music.de>
2020-12-02 13:47:19 +0100

    Make raw-plots more interactive through streamlit-widgets

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Streamlit MNE-Python Demo
-#### A quick demo to see if MNE-Python is running in streamlit
+#### A quick demo to see if [MNE-Python](https://mne.tools/stable/index.html) is running in streamlit for educational purposes
+> A. Gramfort, M. Luessi, E. Larson, D. Engemann, D. Strohmeier, C. Brodbeck, L. Parkkonen, M. Hämäläinen,
+> MNE software for processing MEG and EEG data, NeuroImage, Volume 86, 1 February 2014, Pages 446-460, ISSN 1053-8119,
+> [DOI](https://doi.org/10.1016/j.neuroimage.2013.10.027)
--- main.py
+++ main.py
@@ -2,30 +2,70 @@ import streamlit as st
 import mne
 from os.path import join
 
+
 @st.cache
 def load_raw():
     sample_data_folder = mne.datasets.sample.data_path()
     sample_data_raw_file = join(sample_data_folder, 'MEG', 'sample',
-                                'sample_audvis_filt-0-40_raw.fif')
+                                'sample_audvis_raw.fif')
     loaded_raw = mne.io.read_raw_fif(sample_data_raw_file, preload=True)
 
     return loaded_raw
 
 
+@st.cache
+def filter_raw(rw, hp, lp):
+    with st.spinner(text='Filtering'):
+        rw_filt = rw.copy().filter(hp, lp)
+
+    return rw_filt
+
+
 st.title('MNE-Demo')
 
-highpass = st.sidebar.slider('Set Highpass', min_value=0, max_value=75, value=1)
-lowpass = st.sidebar.slider('Set Lowpass', min_value=0, max_value=75, value=50)
-
 raw = load_raw()
+rounded_sfreq = int(raw.info['sfreq'])
+st.write(f'Loaded raw with a sampling-frequency of {rounded_sfreq}')
 
-raw_figure = raw.plot()
-st.write(raw_figure)
+# Get Filter-Parameters
+highpass = st.sidebar.slider('Set Highpass-Filter', min_value=0, max_value=int(rounded_sfreq/2), value=1)
+lowpass = st.sidebar.slider('Set Lowpass-Filter', min_value=0, max_value=int(rounded_sfreq/2), value=50)
 
-filtering_state = st.text('Filtering data...')
-with st.spinner(text='Filtering'):
-    raw_filtered = raw.copy().filter(highpass, lowpass)
-filtering_state.text('Filtering done!')
+# Set time slider-maximum to length of sample-recording (n time-points/sampling-frequency)
+start = st.sidebar.slider('Time[s]', min_value=0, max_value=int(raw.n_times / raw.info['sfreq']), value=0)
+duration = st.sidebar.number_input('Set Time-Window[s] (max. 100 s)', min_value=1, max_value=100, value=10)
+n_channels = st.sidebar.number_input('Set number of channels to display (max. 50)', min_value=1, max_value=50, value=10)
+channels = st.sidebar.multiselect('Channel-Selection', raw.ch_names, default=raw.ch_names[:20])
 
-figure = raw_filtered.plot_psd(show=False)
-st.write(figure)
+# Pick channels as selected
+raw_picked = raw.copy().pick_channels(channels)
+
+# Filter raw
+raw_filtered = filter_raw(raw_picked, highpass, lowpass)
+
+# Plot-Checkboxes
+show_raw = st.sidebar.checkbox('Show MEG/EEG-Data (unfiltered)')
+show_filt_raw = st.sidebar.checkbox('Show MEG/EEG-Data (filtered)')
+show_psd = st.sidebar.checkbox('Show Power-Spectrum-Density')
+
+if show_raw:
+    st.write('This is the unfiltered MEG/EEG-Data')
+    raw_fig = raw_picked.plot(n_channels=n_channels, duration=duration, start=start, show_scrollbars=False, show=False)
+    st.write(raw_fig)
+else:
+    st.write('Check "Show MEG/EEG-Data (unfiltered)" to plot something')
+
+if show_filt_raw:
+    st.write('This is the filtered MEG/EEG-Data')
+    raw_filt_fig = raw_filtered.plot(n_channels=n_channels, duration=duration, start=start, show_scrollbars=False,
+                                     show=False)
+    st.write(raw_filt_fig)
+else:
+    st.write('Check "Show MEG/EEG-Data (filtered)" to plot something')
+
+if show_psd:
+    st.write('This is the Power-Spectrum-Density of MEG/EEG-Data')
+    psd_fig = raw_filtered.plot_psd(show=False)
+    st.write(psd_fig)
+else:
+    st.write('Check "Show Power-Spectrum-Density" to plot something')
