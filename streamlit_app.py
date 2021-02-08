618561a36c236c0c5e3b18c8599f13a2892b3e2c
marsipu <dev@earthman-music.de>
2021-02-08 18:25:01 +0100

    Reduced functionality to test scalability

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,120 +1,64 @@
-import streamlit as st
-import mne
-import matplotlib
-from matplotlib import pyplot as plt
 from os.path import join
 
-# Pyplot somehow needs to be imported to initialize Matplotlib-Backend and make use of matplotlib.figure.Figure possible
-_ = plt
+import mne
+import streamlit as st
 
 
+# cache is turned off for Raw-Object, maybe if we want to implement reloading after changing some features
+# of the raw-object (n_channels, sfreq), we could implement a custom hash-function for that
 @st.cache(hash_funcs={mne.io.fiff.raw.Raw: lambda _: None}, allow_output_mutation=True)
 def load_raw():
     sample_data_folder = mne.datasets.sample.data_path()
     sample_data_raw_file = join(sample_data_folder, 'MEG', 'sample',
                                 'sample_audvis_raw.fif')
-    raw = mne.io.read_raw_fif(sample_data_raw_file, preload=True)
-    raw.resample(200)
+    raw = mne.io.read_raw_fif(sample_data_raw_file)
+
+    # All the following methods operate in-Place
+    # Reducing data to time window in [s]
+    raw.crop(0, 60)
+
+    # Resampling takes quit a long time, maybe filtering the higher sampling-rate is faster than first resampling
+    #   and then resampling the resampled data
+
+    # # Resampling data to sampling-frequency in [Hz]
+    # st.write('Reducing SampleRate')
+    # loaded_raw.resample(200)
+
+    # Picking only Gradiometer-Channels (example for slow drift in this data)
+    raw.pick_types(meg=False, eeg=True, stim=False, eog=False)
+
+    # Data has to be loaded into memory for filtering afterwards
+    raw.load_data()
 
     return raw
 
 
-@st.cache(hash_funcs={mne.io.fiff.raw.Raw: lambda _: None}, allow_output_mutation=True)
-def pick_channels(raw, channels):
-    raw_picked = raw.copy().pick_channels(channels)
-
-    return raw_picked
-
-
+# cache is turned off for Raw-Object, should only depend on hp/lp-parameters
 @st.cache(hash_funcs={mne.io.fiff.raw.Raw: lambda _: None}, allow_output_mutation=True)
 def filter_raw(raw, hp, lp):
     with st.spinner(text='Filtering'):
-        raw_filtered = raw.copy().filter(hp, lp)
+        raw = raw.copy().filter(hp, lp)
 
-    return raw_filtered
-
-
-@st.cache(hash_funcs={matplotlib.figure.Figure: lambda _: None})
-def plot_raw(raw, n_channels, duration, start):
-    raw_fig = raw.plot(n_channels=n_channels, duration=duration, start=start, show_scrollbars=True, show=False)
-
-    return raw_fig
+    return raw
 
 
-st.title('MNE-Demo')
+st.title('EEG-Filter Demo')
 
-raw = load_raw()
-rounded_sfreq = int(raw.info['sfreq'])
-st.write(f'Loaded raw with a sampling-frequency of {rounded_sfreq}')
+st.sidebar.write('<Erklär-Text>')
 
-# Pick channels as selected
-channels = st.sidebar.multiselect('Channel-Selection (if None, all selected)', raw.ch_names, default=list())
-raw_picked = pick_channels(raw, channels)
-
+loaded_raw = load_raw()
 # Get Filter-Parameters
-highpass = st.sidebar.slider('Set Highpass-Filter', min_value=0, max_value=int(rounded_sfreq / 2), value=1)
-lowpass = st.sidebar.slider('Set Lowpass-Filter', min_value=0, max_value=int(rounded_sfreq / 2), value=50)
-
-# Set time slider-maximum to length of sample-recording (n time-points/sampling-frequency)
-start = st.sidebar.slider('Time[s]', min_value=0, max_value=int(raw.n_times / raw.info['sfreq']), value=0)
-duration = st.sidebar.number_input('Set Time-Window[s] (max. 100 s)', min_value=1, max_value=100, value=10)
-n_channels = st.sidebar.number_input('Set number of channels to display (max. 50)', min_value=1, max_value=50, value=10)
+highpass = st.sidebar.slider('Hochpass-Filter', min_value=0, max_value=100, value=0)
+lowpass = st.sidebar.slider('Tiefpass-Filter', min_value=0, max_value=100, value=100)
 
 # Filter raw
-raw_filtered = filter_raw(raw_picked, highpass, lowpass)
+raw_filtered = filter_raw(loaded_raw, highpass, lowpass)
 
-# Plot-Checkboxes
-show_raw = st.sidebar.checkbox('Show MEG/EEG-Data (unfiltered)')
-show_filt_raw = st.sidebar.checkbox('Show MEG/EEG-Data (filtered)')
-show_psd = st.sidebar.checkbox('Show Power-Spectrum-Density')
+st.write('EEG-Daten gefiltert:')
+filtered_fig = raw_filtered.plot(n_channels=10, duration=60, show_scrollbars=False,
+                                 show=False, title='Filtern von EEG-Daten', remove_dc=False)
+st.write(filtered_fig)
 
-if show_raw:
-    st.write('This is the unfiltered MEG/EEG-Data')
-    raw_fig = plot_raw(raw, n_channels=n_channels, duration=duration, start=start)
-    st.write(raw_fig)
-    # Add plot-buttons
-    raw_col1, raw_col2, raw_col3, raw_col4 = st.beta_columns(4)
-    with raw_col1:
-        if st.button('Raw up'):
-            raw_fig.canvas.key_press_event('up')
-    with raw_col2:
-        if st.button('Raw down'):
-            raw_fig.canvas.key_press_event('down')
-    with raw_col3:
-        if st.button('Raw left'):
-            raw_fig.canvas.key_press_event('shift+left')
-    with raw_col4:
-        if st.button('Raw right'):
-            raw_fig.canvas.key_press_event('shift+right')
-    st.write('(Buttons are falling one step behind)')
-else:
-    st.write('Check "Show MEG/EEG-Data (unfiltered)" to plot something')
-
-if show_filt_raw:
-    st.write('This is the filtered MEG/EEG-Data')
-    raw_filt_fig = plot_raw(raw_filtered, n_channels=n_channels, duration=duration, start=start)
-    st.write(raw_filt_fig)
-    # Add plot-buttons
-    raw_filt_col1, raw_filt_col2, raw_filt_col3, raw_filt_col4 = st.beta_columns(4)
-    with raw_filt_col1:
-        if st.button('Filt up'):
-            raw_filt_fig.canvas.key_press_event('up')
-    with raw_filt_col2:
-        if st.button('Filt down'):
-            raw_filt_fig.canvas.key_press_event('down')
-    with raw_filt_col3:
-        if st.button('Filt left'):
-            raw_filt_fig.canvas.key_press_event('shift+left')
-    with raw_filt_col4:
-        if st.button('Filt right'):
-            raw_filt_fig.canvas.key_press_event('shift+right')
-    st.write('(Buttons are falling one step behind)')
-else:
-    st.write('Check "Show MEG/EEG-Data (filtered)" to plot something')
-
-if show_psd:
-    st.write('This is the Power-Spectrum-Density of MEG/EEG-Data')
-    psd_fig = raw_filtered.plot_psd(show=False)
-    st.write(psd_fig)
-else:
-    st.write('Check "Show Power-Spectrum-Density" to plot something')
+st.write('Frequenzspektrum:')
+psd_fig = raw_filtered.plot_psd(show=False)
+st.write(psd_fig)
